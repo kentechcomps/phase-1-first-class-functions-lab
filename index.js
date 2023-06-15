e12ece2abed77435150b032032bb79639dacff3e
user <mutukukennedy34@gmail.com>
2023-06-15 18:47:48 +0300

    functionslab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+
+const returnFirstTwoDrivers = function (drivers){
+    for (let item of drivers){
+        console.log(drivers.item[0] , [1])
+    }
+}
+const returnLastTwoDrivers = function (drivers){
+    for (let item of drivers){
+        console.log(drivers.item[3] , [4])
+    }
+}
+const selectDifferentDrivers = [function (returnFirstTwoDrivers){
+    
+    return returnFirstTwoDrivers[0]
+} , function (returnLastTwoDrivers){
+    
+    return returnLastTwoDrivers[0]} ]
+
+    function createFareMultiplier(){
+         const fareMultiplier =createFareMultiplier(2)
+    }
+
+    function fareDoubler ( fair ){
+    console.log(fair * 2);
+    }
+    function fareTripler(fair){
+        console.log(fair *3);
+    }
+    function selectDifferentDrivers( drivers , fun){
+        return FirstTwoDrivers();
+    }
+   
+
+
